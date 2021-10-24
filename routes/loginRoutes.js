28f975f20ec66560bab4b81dd285ca6ff1b2dfbd
Arthur <rheiquelogor@gmail.com>
2021-10-24 11:18:44 +0200

    directory for utils

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -2,9 +2,8 @@
 const express = require('express');
 //router
 const router = express.Router();
-
+//get google token 
 const getGGToken = require('../googleAuth/google-token');
-
 
 router.post('/', (req, res) => {
   getGGToken(req, res);
